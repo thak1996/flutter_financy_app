f6dd875da8059959b6f88a860f0eddf7eb2932c4
Franklyn Viana <franklyn_vs_@hotmail.com>
2022-11-22 12:26:33 -0300

    fonts, splash + onboarding

---
+++ lib/common/widgets/primary_button.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_financy_app/common/constants/app_colors.dart';
+import 'package:flutter_financy_app/common/constants/app_text_styles.dart';
+
+class PrimaryButton extends StatelessWidget {
+  PrimaryButton({
+    Key? key,
+    this.onPressed,
+    required this.text,
+  }) : super(key: key);
+  final BorderRadius _borderRadius = BorderRadius.circular(40);
+  final VoidCallback? onPressed;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      borderRadius: _borderRadius,
+      child: Ink(
+        height: 64,
+        width: MediaQuery.of(context).size.width * .75,
+        decoration: BoxDecoration(
+          borderRadius: _borderRadius,
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: onPressed != null
+                ? AppColors.greenGradiente
+                : AppColors.greyGradiente,
+          ),
+        ),
+        child: InkWell(
+          borderRadius: _borderRadius,
+          onTap: onPressed,
+          child: Align(
+            child: Text(
+              text,
+              style: AppTextStyles.mediumText18.apply(color: AppColors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
